ebe877c5183a83f3a0f651d8e566b3e7bf10a131
Haku Hal <hakuhal.snow@gmail.com>
2016-11-29 22:14:03 +0800

    change to use the js to set the screen to rotate 90, and add the gravity listener/通过js做了横屏适配，另外做了重力监听

--- js/main.js
+++ js/main.js
@@ -1,3 +1,59 @@
 /**
  * Created by hakuh on 2016/11/28.
  */
+LGlobal={};
+// 横屏设置，以及重力监听
+document.addEventListener("DOMContentLoaded",function(){
+    setScreen();
+});
+
+function setScreen(){
+    var loaded=false;
+    roScreen();
+    function roScreen(){
+       /* var b=document.getElementsByTagName("body")[0];
+        var t=document.getElementsByTagName("html")[0];*/
+        var lo=document.getElementsByClassName("pageLoadBox")[0];
+        var ps=document.getElementsByClassName("page")[0];
+        var lock=document.getElementsByClassName("lock")[0];
+        setTimeout(function(){
+            var w=LGlobal.w=window.innerWidth;
+            var h=LGlobal.h=window.innerHeight;
+            ps.style.width=lo.style.width=h+"px";
+            ps.style.height=lo.style.height=w+"px";
+            lo.style.transformOrigin=lo.style.webkitTransformOrigin="left top";
+            ps.style.transformOrigin=ps.style.webkitTransformOrigin="left top";
+            lo.style.transform=lo.style.webkitTransform="translate("+window.innerWidth+"px,0) rotate(90deg)";
+            ps.style.transform=ps.style.webkitTransform="translate("+window.innerWidth+"px,0) rotate(90deg)";
+            window.addEventListener("resize",change);
+
+            // 监听锁屏事件
+            function change(){
+
+                if(window.innerWidth<window.innerHeight){
+                    ps.style.width=lo.style.width=h+"px";
+                    ps.style.height=lo.style.height=w+"px";
+                    lo.style.transform=lo.style.webkitTransform="translate("+window.innerWidth+"px,0) rotate(90deg)";
+                    ps.style.transform=ps.style.webkitTransform="translate("+window.innerWidth+"px,0) rotate(90deg)";
+                    if(loaded){
+                        loaded=false;
+                        lock.style.display="none";
+                    }
+
+                }
+                if(window.innerWidth>window.innerHeight){
+                    if(!loaded&&!LGlobal.focus){
+                        lock.style.display="block";
+                        loaded=true;
+                    }
+                    ps.style.width=lo.style.width=w+"px";
+                    ps.style.height=lo.style.height=h+"px";
+                    ps.style.left=lo.style.left="0px";
+                    ps.style.top=lo.style.top="0px";
+                    lo.style.transform=lo.style.webkitTransform="rotate(0deg)";
+                    ps.style.transform=ps.style.webkitTransform="rotate(0deg)";
+                }
+            }
+        },100);
+    }
+}
