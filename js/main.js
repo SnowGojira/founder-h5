7085179def1439f26e14eb52a67433b89d6275d2
Haku Hal <hakuhal.snow@gmail.com>
2016-12-05 01:43:34 +0800

    完成了下半题的逻辑/complete the second quiz logic

--- js/main.js
+++ js/main.js
@@ -174,7 +174,7 @@ function bubble(){
             anim : [0,2,'anim']
         }
 
-    }
+    };
 
     var spriteSheet2 = new createjs.SpriteSheet(data);
     var img1 = new createjs.Sprite(spriteSheet2, 'anim');
@@ -247,7 +247,7 @@ function snow(){
             anim : [0,2,'anim']
         }
 
-    }
+    };
 
     var spriteSheet2 = new createjs.SpriteSheet(data);
     var img1 = new createjs.Sprite(spriteSheet2, 'anim');
@@ -284,7 +284,7 @@ function cloud(){
             anim : [0,2,'anim']
         }
 
-    }
+    };
 
     var spriteSheet2 = new createjs.SpriteSheet(data);
     var img1 = new createjs.Sprite(spriteSheet2, 'anim');
@@ -320,7 +320,7 @@ function signal(){
             anim : [0,5,'anim']
         }
 
-    }
+    };
 
     var spriteSheet2 = new createjs.SpriteSheet(data);
     var img1 = new createjs.Sprite(spriteSheet2, 'anim');
@@ -361,7 +361,7 @@ function heart(){
             anim : [0,2,'anim']
         }
 
-    }
+    };
 
     var spriteSheet2 = new createjs.SpriteSheet(data);
     var img1 = new createjs.Sprite(spriteSheet2, 'anim');
@@ -400,7 +400,7 @@ function flower(canvas){
             anim : [0,2,'anim']
         }
 
-    }
+    };
 
     var spriteSheet2 = new createjs.SpriteSheet(data);
     var img1 = new createjs.Sprite(spriteSheet2, 'anim');
@@ -438,7 +438,7 @@ function money(canvas,path){
             anim : [0,1,'anim']
         }
 
-    }
+    };
 
     var spriteSheet2 = new createjs.SpriteSheet(data);
     var img1 = new createjs.Sprite(spriteSheet2, 'anim');
@@ -458,19 +458,23 @@ var Title1= new createjs.Bitmap("./images/page5/title1.png");
 var Title2= new createjs.Bitmap("./images/page5/title2.png");
 var Title1_r= new createjs.Bitmap("./images/page5/title1_r.png");
 var Title2_r= new createjs.Bitmap("./images/page5/title2_r.png");
+var train= new createjs.Bitmap("./images/train.png");
+var railway = new createjs.Bitmap("./images/railway.png");
+var tweenTrain5;
+var tweenTitle1_5;
+var tweenTitle2_5;
+var tweenTitle1_5_r;
+var tweenTitle2_5_r;
 
 function train5In() {
 
     stage5.canvas.width=h;
     stage5.canvas.height=w;
-
     /*图片适配比例*/
     var original = w*0.275;
     var scale = original/203;
     var positonY=0.489*w;
     // console.log(w+" "+original+" "+scale+" "+positonY);
-    var train= new createjs.Bitmap("./images/train.png");
-    var railway = new createjs.Bitmap("./images/railway.png");
 
 
     train.scaleX=scale;
@@ -514,23 +518,28 @@ function train5In() {
     stage5.addChild(Title1);
     stage5.addChild(Title2);
 
-    createjs.Ticker.setFPS(30);
+    createjs.Ticker.setFPS(20);
     createjs.Ticker.on('tick',stage5);
 
-    createjs.Tween.get(train, {loop: false})
-        .to({x: 200}, 10000, createjs.Ease.getPowInOut(4));
 
-    createjs.Tween.get(Title1, {loop: false})
-        .to({x: 200}, 10000, createjs.Ease.getPowInOut(4));
+    createjs.Sound.on("fileload", playEnter, this);
+    tweenTrain5=createjs.Tween.get(train, {loop: false})
+        .to({x: -5000}, 25000, createjs.Ease.getPowInOut(1));
+    setTimeout(function () {
+        tweenTrain5.setPaused(true);
 
-    createjs.Tween.get(Title2, {loop: false})
-        .to({x: 200}, 10000, createjs.Ease.getPowInOut(4));
+    },3000);
+    tweenTitle1_5=createjs.Tween.get(Title1, {loop: false})
+        .to({x: 150}, 1000, createjs.Ease.getPowOut(4));
 
-    createjs.Tween.get(Title1_r, {loop: false})
-        .to({x: 200}, 10000, createjs.Ease.getPowInOut(4));
+    tweenTitle2_5=createjs.Tween.get(Title2, {loop: false})
+        .to({x: 150}, 1000, createjs.Ease.getPowOut(4));
 
-    createjs.Tween.get(Title2_r, {loop: false})
-        .to({x: 200}, 10000, createjs.Ease.getPowInOut(4));
+    tweenTitle1_5_r=createjs.Tween.get(Title1_r, {loop: false})
+        .to({x: 150}, 1000, createjs.Ease.getPowOut(4));
+
+    tweenTitle2_5_r=createjs.Tween.get(Title2_r, {loop: false})
+        .to({x: 150}, 1000, createjs.Ease.getPowOut(4));
 }
 train5In();
 
@@ -563,73 +572,69 @@ function handleFileProgress(event){//加载中函数
 
 
 //The passcode to check against.
-var password_bu = [6,8,9,2];
-var password_bian = [2,6,9,9,3,2,4,0];
+var password_bubian = [6,8,9,2,2,6,9,9,3,2,4,0];
 //The values that the user enters while attempting to unlock the phone
 var enteredPass = [];
-var isTitle1 = false;
-var isTitle2 = false;
 
 /*Functions to enter the numbers to enteredPass[]*/
 function lock(a) {
-
     var Choice = $('#key5'+a).attr('data-choice');
     console.log("点击的choice："+Choice);
     enteredPass.push(Choice);
     console.log("enterPass的长度："+enteredPass.length);
-    checkPasscode_bu('#key5',password_bu);
+    checkPasscode_bu('#key5',password_bubian);
 }
-
 
 /*拼写的规则*/
 var booleanArr = [];
 function checkPasscode_bu(key,password) {
     /*Runs through each of the password values. If the arrays match, it triggers the unlocked() function */
-    /*if(enteredPass.length>password.length){
-        return;
-    }*/
 
     if (enteredPass.length>password.length){
-        resetAll();
+         return;
     }else{
         for(var i = 0; i<password.length;i++){
             if (enteredPass[i]==password[i]){
-                if(i==password.length-1){
+                booleanArr.push(true);
+                if (i==3){
+                    Title1.visible=false;
+                    Title1_r.visible=true;
+                }
+                else if(i==password.length-1){
+                    Title2.visible=false;
+                    Title2_r.visible=true;
                     unlocked();
-                    // isTitle1=true;
-                    booleanArr.push(true);
+
                 }else{
-                    // isTitle1=true;
-                    booleanArr.push(true);
+
                 }
             }else{
-                // isTitle1=false;
+
                 booleanArr.push(false);
             }
         }
     }
 
     if(enteredPass.length>0 ){
-        console.log(booleanArr);
-        console.log(booleanArr[enteredPass.length-1]+"");
-
+        console.log("enteredPass里面开始有内容的时候："+booleanArr);
+        console.log("当前指针对应的boolean值是："+booleanArr[enteredPass.length-1]+"");
         if (booleanArr[enteredPass.length-1]){
             checkRight(key+enteredPass[enteredPass.length-1]);
         }else {
             checkWrong(key+enteredPass[enteredPass.length-1]);
+            enteredPass.pop();
         }
+        //需要清空，重新加入新的遍历array
         booleanArr=[];
     }else{
-
     }
 
-    console.log(enteredPass);
-    console.log(booleanArr);
-
+    console.log("enteredPass的状态："+enteredPass);
 
 }
 
 function checkRight(key){
+    playRight();
     $(key+'_r').show();
     setTimeout(function () {
         $(key+'_r').hide();
@@ -638,25 +643,78 @@ function checkRight(key){
 }
 
 function checkWrong(key){
-    resetAll();
+    playWrong();
     $(key+'_w').show();
     setTimeout(function () {
         $(key+'_w').hide();
     },100);
-
     console.log(key+'_w');
-
 }
+
+
 
 /*Displays all the correct stuff, then moves on to homeScreen()*/
 function unlocked() {
-    Title1.visible=false;
-    Title1_r.visible=true;
     console.log("Your Phone is Unlocked");
+    setTimeout(function () {
+        playOut();
+    },10);
+    createjs.Tween.get(train, {loop: false})
+        .to({x: -10000}, 150, createjs.Ease.getPowOut(4));
+
+    createjs.Tween.get(Title1, {loop: false})
+        .to({x: -10000}, 150, createjs.Ease.getPowOut(4));
+
+    createjs.Tween.get(Title2, {loop: false})
+        .to({x: -10000}, 150, createjs.Ease.getPowOut(4));
+
+    createjs.Tween.get(Title1_r, {loop: false})
+        .to({x: -10000}, 150, createjs.Ease.getPowOut(4));
+
+    createjs.Tween.get(Title2_r, {loop: false})
+        .to({x: -10000}, 150, createjs.Ease.getPowOut(4));
 
 }
 
 function resetAll() {
+    Title1.visible=true;
+    Title1_r.visible=false;
+
+    Title2.visible=true;
+    Title2_r.visible=false;
     enteredPass = [];
     console.log("清零了请重新输入");
+}
+
+//声音文件的播放方法
+function playWrong() {
+    createjs.Sound.registerSound({src:"asset/audio/wrong.mp3", id:"wrong"});
+    createjs.Sound.play("wrong");
+}
+
+function playRight(){
+    createjs.Sound.registerSound({src:"asset/audio/right.mp3", id:"right"});
+    createjs.Sound.play("right");
+}
+function playOut() {
+    createjs.Sound.registerSound({src:"asset/audio/out.mp3", id:"out"});
+    createjs.Sound.play("out");
+}
+
+function playEnter() {
+    createjs.Sound.registerSound({src:"asset/audio/enter.mp3", id:"enter"});
+    createjs.Sound.play("enter");
+}
+function playStart() {
+    createjs.Sound.registerSound({src:"asset/audio/start.mp3", id:"start"});
+    createjs.Sound.play("start");
+}
+function playRun(){
+    createjs.Sound.registerSound({src:"asset/audio/run.mp3", id:"run"});
+    createjs.Sound.play("run");
+}
+
+function playCorrect() {
+    createjs.Sound.registerSound({src:"asset/audio/correct.mp3", id:"correct"});
+    createjs.Sound.play("correct");
 }
