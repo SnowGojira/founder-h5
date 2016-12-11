f73427c5d2df6e74bb8c217e41b7d7ae6769e50d
SirSnow <hakuhal.snow@gmail.com>
2016-12-11 20:44:38 +0800

    Add some comments

--- ajax_getconfig.php
+++ ajax_getconfig.php
@@ -1,6 +1,6 @@
 <?php
 require_once "jssdk1.php";
-$jssdk = new JSSDK("wxc6d64ef0eca24344", "64c9340300279bf7e2f5d21e3694fea5",'http://weixin.assemblemedia.cn/founder/index.html');
+$jssdk = new JSSDK("your wxID", "your wxPasscode",'your static Network');
 $signPackage = $jssdk->GetSignPackage();
 echo json_encode($signPackage);
 ?>
