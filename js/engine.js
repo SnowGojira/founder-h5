6b058877185283ae28fd7682d52503953108d4e4
Haku <hakuhal.snow@gmail.com>
2019-08-20 17:14:37 +0800

    change the structures into script.js and engine.js

--- js/engine.js
+++ js/engine.js
@@ -78,12 +78,153 @@ var audio_wrong = new Sound('audio_wrong'),
 
 // renderAudios();
 ////////////////////////functions' reference//////////////////////////////
-// function renderAudios (){
-//      audioArr.forEach(function (audio) {
-//          audio.render();
-//      });
-// };
 
+function preload (handleFileProgress,handleComplete){
+    let manifest = [
+        {src: 'asset/audio/out.mp3', id: 'sona2'},
+        {src: 'asset/audio/right.mp3', id: 'sona3'},
+        {src: 'asset/audio/running.mp3', id: 'sona4'},
+        {src: 'asset/audio/start.mp3', id: 'sona5'},
+        {src: 'asset/audio/wrong.mp3', id: 'sona6'},
+        {src: 'asset/audio/bg.mp3', id: 'sona7'},
+        {src: 'asset/font/timing-light.TTF', id: 'font1'},
+        {src: 'asset/font/writing-light.TTF', id: 'font2'},
+
+        {src: 'images/page1/btn.png', id: 'p12'},
+        {src: 'images/page1/bubble.png', id: 'p13'},
+        {src: 'images/page1/hint.png', id: 'p15'},
+        {src: 'images/page1/title.png', id: 'p16'},
+        {src: 'images/page1/fbgt.png', id: 'p18'},
+        {src: 'images/page1/bgt1.png', id: 'p19'},
+
+        {src: 'images/page2/snow1.png', id: 'p20'},
+        {src: 'images/page2/fa1.png', id: 'p21'},
+        {src: 'images/page2/fa2.png', id: 'p22'},
+        {src: 'images/page2/fa3.png', id: 'p23'},
+        {src: 'images/page2/fa4.png', id: 'p24'},
+        {src: 'images/page2/fa5.png', id: 'p25'},
+        {src: 'images/page2/fa6.png', id: 'p26'},
+
+
+        {src: 'images/page3/cloud.png', id: 'p30'},
+        {src: 'images/page3/fa1.png', id: 'p31'},
+        {src: 'images/page3/fa2.png', id: 'p32'},
+        {src: 'images/page3/fa3.png', id: 'p33'},
+        {src: 'images/page3/fa4.png', id: 'p34'},
+        {src: 'images/page3/fa5.png', id: 'p35'},
+        {src: 'images/page3/fa6.png', id: 'p36'},
+        {src: 'images/page3/fa7.png', id: 'p37'},
+        {src: 'images/page3/signal.png', id: 'p38'},
+
+        {src: 'images/page4/heart.png', id: 'p40'},
+        {src: 'images/page4/fa1.png', id: 'p41'},
+        {src: 'images/page4/fa2.png', id: 'p42'},
+        {src: 'images/page4/fa3.png', id: 'p43'},
+        {src: 'images/page4/fa4.png', id: 'p44'},
+        {src: 'images/page4/fa5.png', id: 'p45'},
+        {src: 'images/page4/fa6.png', id: 'p46'},
+        {src: 'images/page4/fa7.png', id: 'p47'},
+        {src: 'images/page4/fa8.png', id: 'p48'},
+
+        {src: 'images/page5/flower.png', id: 'p50'},
+        {src: 'images/page5/fa1.png', id: 'p51'},
+        {src: 'images/page5/fa2.png', id: 'p52'},
+        {src: 'images/page5/fa3.png', id: 'p53'},
+        {src: 'images/page5/fa4.png', id: 'p54'},
+        {src: 'images/page5/fa5.png', id: 'p55'},
+        {src: 'images/page5/fa6.png', id: 'p56'},
+        {src: 'images/page5/fa7.png', id: 'p57'},
+        {src: 'images/page5/money.png', id: 'p58'},
+
+        {src: 'images/page6/flag.png', id: 'p63'},
+        {src: 'images/page6/solgan1.png', id: 'p64'},
+        {src: 'images/page6/solgan2.png', id: 'p65'},
+        {src: 'images/page6/check.png', id: 'p60'},
+        {src: 'images/page6/money.png', id: 'p61'},
+
+        {src: 'images/page7/bg.png', id: 'p71'},
+        {src: 'images/page7/border1.png', id: 'p72'},
+        {src: 'images/page7/share.png', id: 'p73'},
+        {src: 'images/page7/btn1.png', id: 'p74'},
+        {src: 'images/page7/btn2.png', id: 'p75'},
+        {src: 'images/page7/btn3.png', id: 'p76'},
+
+        {src: 'images/railway.png', id: 'p1'},
+        {src: 'images/timer.png', id: 'p2'},
+        {src: 'images/train.png', id: 'p3'},
+
+        {src: 'images/title1/0.png', id: 't10'},
+        {src: 'images/title1/1.png', id: 't11'},
+        {src: 'images/title1/2.png', id: 't12'},
+        {src: 'images/title1/3.png', id: 't13'},
+        {src: 'images/title1/4.png', id: 't14'},
+        {src: 'images/title1/5.png', id: 't15'},
+        {src: 'images/title1/6.png', id: 't16'},
+        {src: 'images/title1/7.png', id: 't17'},
+        {src: 'images/title1/8.png', id: 't18'},
+        {src: 'images/title1/9.png', id: 't19'},
+        {src: 'images/title1/10.png', id: 't110'},
+        {src: 'images/title1/11.png', id: 't111'},
+        {src: 'images/title1/12.png', id: 't112'},
+        {src: 'images/title1/13.png', id: 't113'},
+        {src: 'images/title1/14.png', id: 't114'},
+
+        {src: 'images/title2/0.png', id: 't20'},
+        {src: 'images/title2/1.png', id: 't21'},
+        {src: 'images/title2/2.png', id: 't22'},
+        {src: 'images/title2/3.png', id: 't23'},
+        {src: 'images/title2/4.png', id: 't24'},
+        {src: 'images/title2/5.png', id: 't25'},
+        {src: 'images/title2/6.png', id: 't26'},
+        {src: 'images/title2/7.png', id: 't27'},
+        {src: 'images/title2/8.png', id: 't28'},
+        {src: 'images/title2/9.png', id: 't29'},
+        {src: 'images/title2/10.png', id: 't210'},
+        {src: 'images/title2/11.png', id: 't211'},
+        {src: 'images/title2/12.png', id: 't212'},
+        {src: 'images/title2/13.png', id: 't213'},
+        {src: 'images/title2/14.png', id: 't214'},
+
+        {src: 'images/title3/0.png', id: 't30'},
+        {src: 'images/title3/1.png', id: 't31'},
+        {src: 'images/title3/2.png', id: 't32'},
+        {src: 'images/title3/3.png', id: 't33'},
+        {src: 'images/title3/4.png', id: 't34'},
+        {src: 'images/title3/5.png', id: 't35'},
+        {src: 'images/title3/6.png', id: 't36'},
+        {src: 'images/title3/7.png', id: 't37'},
+        {src: 'images/title3/8.png', id: 't38'},
+        {src: 'images/title3/9.png', id: 't39'},
+        {src: 'images/title3/10.png', id: 't310'},
+
+
+        {src: 'images/title4/0.png', id: 't40'},
+        {src: 'images/title4/1.png', id: 't41'},
+        {src: 'images/title4/2.png', id: 't42'},
+        {src: 'images/title4/3.png', id: 't43'},
+        {src: 'images/title4/4.png', id: 't44'},
+        {src: 'images/title4/5.png', id: 't45'},
+        {src: 'images/title4/6.png', id: 't46'},
+        {src: 'images/title4/7.png', id: 't47'},
+        {src: 'images/title4/8.png', id: 't48'},
+        {src: 'images/title4/9.png', id: 't49'},
+        {src: 'images/title4/10.png', id: 't410'},
+        {src: 'images/title4/11.png', id: 't411'},
+        {src: 'images/title4/12.png', id: 't412'}
+
+    ];
+    //预加载
+    loader = new createjs.LoadQueue(false);
+    // 关键！----设置并发数
+    loader.setMaxConnections(100);
+// 关键！---一定要将其设置为 true, 否则不起作用。
+    loader.maintainScriptOrder=true;
+    loader.installPlugin(createjs.Sound);
+    loader.loadManifest(manifest);
+    loader.addEventListener('progress', handleFileProgress);//加载完成 调用handleFileProgress函数
+    loader.addEventListener('complete', handleComplete);//加载完成 调用handleComplete函数
+
+};
 
 
 document.addEventListener('DOMContentLoaded', function () {
